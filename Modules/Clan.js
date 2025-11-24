c7ba5a43372a5e56680ef0638749b18b2e429556
Hacke-boy <ldorce30@gmail.com>
2025-11-24 15:29:28 -0500

    Add handleClanWelcome function for clan greeting

---
+++ Modules/Clan.js
@@ -0,0 +1,5 @@
+export function handleClanWelcome(sock, chat) {
+    sock.sendMessage(chat, {
+        text: `👋 *Bienvenue dans le clan !*\n\n🔰 RÈG GUILD LAN :\n- Respè tout moun\n- Chwazi rôle ou\n- Pa flood\n- Pa insulte`
+    })
+}
